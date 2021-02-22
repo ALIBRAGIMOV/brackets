bae464c468721a471597c0d0b22ac47ee38b799f
Ali <ialimagomed@mail.ru>
2021-02-22 19:26:28 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let a = []
+  let b = []
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    a[i] = bracketsConfig[i][0]
+    b[i] = bracketsConfig[i][1]
+  }
+  let arr = str.split('')
+  for (let i = 0; i < arr.length; i++) {
+    for (item in a) {
+      if (arr[i] == a[item]) {
+        if (arr[i + 1] == b[item]) {
+          arr.splice(i, 2)
+          i = -1
+          break
+        }
+      }
+    }
+  }
+  return arr.length == 0 ? true : false
 }
